fadb023a866c77ad49f6debeb92b11749ff11c7c
Csedge <liangfen@oregonstate.edu>
2019-06-12 16:48:13 -0700

    Update 493Final.sql

--- db-init/493Final.sql
+++ db-init/493Final.sql
@@ -2,10 +2,10 @@
 -- version 4.4.15.10
 -- https://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: 2019-06-06 06:29:22
--- 服务器版本： 10.1.26-MariaDB
--- PHP Version: 7.1.5
+-- 主機: localhost
+-- 產生時間： 2019 年 06 月 13 日 07:47
+-- 伺服器版本: 10.1.26-MariaDB
+-- PHP 版本： 7.1.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `493Final`
+-- 資料庫： `493Final`
 --
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `assignments`
+-- 資料表結構 `assignments`
 --
 
 CREATE TABLE IF NOT EXISTS `assignments` (
@@ -32,12 +32,20 @@ CREATE TABLE IF NOT EXISTS `assignments` (
   `title` text NOT NULL,
   `points` int(11) NOT NULL,
   `due` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- 資料表的匯出資料 `assignments`
+--
+
+INSERT INTO `assignments` (`id`, `courseid`, `title`, `points`, `due`) VALUES
+(1, 2, 'Install linux on your computer', 40, '2019-06-07 00:00:00'),
+(2, 2, 'Keiyh', 100, '2019-06-07 00:00:00');
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `courses`
+-- 資料表結構 `courses`
 --
 
 CREATE TABLE IF NOT EXISTS `courses` (
@@ -47,12 +55,35 @@ CREATE TABLE IF NOT EXISTS `courses` (
   `title` text NOT NULL,
   `term` text NOT NULL,
   `instructor` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- 資料表的匯出資料 `courses`
+--
+
+INSERT INTO `courses` (`id`, `subject`, `number`, `title`, `term`, `instructor`) VALUES
+(1, 'CS', 150, 'CPP Program', 'SP19', 2),
+(2, 'CS', 171, 'Python', 'SP19', 2),
+(3, 'MTH', 271, 'Matrix Maths', 'SP19', 2),
+(4, 'ECE', 271, 'Digital Ocean', 'Su18', 3),
+(5, 'ECE', 291, 'CuteAnankke', 'Su18', 3);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `enrollment`
+--
+
+CREATE TABLE IF NOT EXISTS `enrollment` (
+  `id` int(11) NOT NULL,
+  `courseid` int(11) NOT NULL,
+  `studentid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `submissions`
+-- 資料表結構 `submissions`
 --
 
 CREATE TABLE IF NOT EXISTS `submissions` (
@@ -66,7 +97,7 @@ CREATE TABLE IF NOT EXISTS `submissions` (
 -- --------------------------------------------------------
 
 --
--- 表的结构 `users`
+-- 資料表結構 `users`
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
@@ -78,54 +109,65 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indexes for dumped tables
+-- 已匯出資料表的索引
 --
 
 --
--- Indexes for table `assignments`
+-- 資料表索引 `assignments`
 --
 ALTER TABLE `assignments`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `courses`
+-- 資料表索引 `courses`
 --
 ALTER TABLE `courses`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `submissions`
+-- 資料表索引 `enrollment`
+--
+ALTER TABLE `enrollment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `submissions`
 --
 ALTER TABLE `submissions`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- 資料表索引 `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- 在匯出的資料表使用 AUTO_INCREMENT
 --
 
 --
--- AUTO_INCREMENT for table `assignments`
+-- 使用資料表 AUTO_INCREMENT `assignments`
 --
 ALTER TABLE `assignments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT for table `courses`
+-- 使用資料表 AUTO_INCREMENT `courses`
 --
 ALTER TABLE `courses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- 使用資料表 AUTO_INCREMENT `enrollment`
+--
+ALTER TABLE `enrollment`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `submissions`
+-- 使用資料表 AUTO_INCREMENT `submissions`
 --
 ALTER TABLE `submissions`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `users`
+-- 使用資料表 AUTO_INCREMENT `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
