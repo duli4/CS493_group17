cb3b4cbf856121f55ce2b1223499b5e39009215f
Csedge <liangfen@oregonstate.edu>
2019-06-13 20:11:59 -0700

    Update courses.js

--- api/courses.js
+++ api/courses.js
@@ -225,4 +225,72 @@ router.post('/', requireAuthentication, async (req, res,next) => {
 });
 
 
+router.get('/:id', async(req, res, next) => {
+  try{
+    const course = await getCourseById(parseInt(req.params.id));
+    if (user) {
+      res.status(200).send(course);
+    } else{
+      res.status(404).send({
+        error: "No such course."
+      });
+    }
+  } catch (err){
+    console.log("Course ERROR: ", err);
+    res.status(404).send({
+      error: "Unable to fetch course."
+    });
+  }
+});
+
+router.put('/:id', requireAuthentication, async(req,res,next) => {
+  const userid = await getUserByEmail(req.user);
+  const userRole = await getRoleByemail(req.user);
+  const teach_id = getCourseOfInstructor(id);
+  if (validateAgainstSchema(req.body, CourseSchema)) {
+    if(userRole.role == 'admin' || (userRole.role == 'instructor' && userRole.id == teach_id)) {
+      try{
+        const updated = updatecourseInfo(parseInt(req.params.id), req.body);
+        res.status(200).send({});
+      }catch(err){
+        console.log("put method: ",err);
+        res.status(404).send({
+          error: "Unable to update course."
+        });
+      }
+    }
+    else{
+      res.status(403).send({
+        error: "Error manager to update course information!"
+      });
+    }
+  }else{
+    res.status(400).send({
+      error: "Invalid body of course."
+    });
+  }
+});
+
+router.delete('/:id', requireAuthentication, async(req,res,next) => {
+  const userRole = await getRoleByemail(req.user);
+  if(userRole.role == 'admin'){
+    try{
+      const courseid = deleteCourse(id);
+      const enrolldelete = deleteEnroll(id);
+      console.log("enroll delete:", enrolldelete, "course id: ", courseid);
+      res.status(204).end();
+    }
+    catch(err){
+      console.log("in delete endpoint: ", err);
+      res.status(400).send({
+        error: "Error to delete course. "
+      });
+    }
+  }else{
+    res.status(403).send({
+      error: "No power to delete the course"
+    });
+  }
+});
+
 module.exports = router;
